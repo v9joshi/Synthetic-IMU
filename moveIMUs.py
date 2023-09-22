4e829a2d48913738ec1b48fc4460c42c9c778c3f
Varun Joshi <joshi.142@osu.edu>
2023-09-22 15:19:14 -0400

    Update moveIMUs.py
    Modified to use cylindrical coordinates for placement translation errors.
    Added IMU placement rotational errors.

--- moveIMUs.py
+++ moveIMUs.py
@@ -13,17 +13,32 @@ if not os.path.exists(modelFilePath):
 numVirtualSubjects = 2 # Making a thousand is equally easy
 
 # Load the base model
-baseModelName  = "RajagopalModel.osim"
+baseModelName  = "./BaseModels/RajagopalModel.osim"
 
 # How much do we want to translate the IMUs by?
-meanPosErrorX    = 0   
+# Depth/Distance from bone
+meanPosErrorX    = 0
 stddevPosErrorX  = 0.02
 
-meanPosErrorY   = 0
-stddevPosErrorY = 0.02
+# Rotational error in placement
+minPlacementErrorAngle      = 0
+maxPlacementErrorAngle      = 2*numpy.pi
 
-meanPosErrorZ   = 0
-stddevPosErrorZ = 0.02
+minPlacementErrorRadius     = 0
+maxPlacementErrorRadius     = 0.001
+
+# Rotational error in orientation using XYZ Euler angles
+# Rotate in 
+minOrientationErrorXAngle    =  0*numpy.pi
+maxOrientationErrorXAngle    =  0*numpy.pi
+
+# Rotate in the sagittal plane
+minOrientationErrorYAngle    =  0*numpy.pi  
+maxOrientationErrorYAngle    =  0*numpy.pi
+
+# Rotate in the frontal plane
+minOrientationErrorZAngle    =  -0.25*numpy.pi
+maxOrientationErrorZAngle    =   0.25*numpy.pi
 
 # Load the base model
 baseModel = opensim.Model(baseModelName)
@@ -33,7 +48,6 @@ IMUContainer = baseModel.getMiscModelComponentSet()
 numIMUFrames = IMUContainer.getSize()
 
 frameNames = []
-
 for iter in range(numIMUFrames):
     currFrame = IMUContainer.get(iter).getSocket('frame').getConnecteePath()
     frameNames.append(currFrame)
@@ -53,20 +67,40 @@ for currSubj in range(numVirtualSubjects):
         # Get the imu frame from the body
         IMUFrame = baseModel.getComponent(imuFrame)
 
-        # Find the position of the IMU frame
+        # Find the position and orientation of the IMU frame
         IMUpos = IMUFrame.get_translation().to_numpy()
+        IMUang = IMUFrame.get_orientation().to_numpy()
 
         # How much should we move the IMU by?        
-        randomMovX = numpy.random.normal(meanPosErrorX, stddevPosErrorX, 1)
-        randomMovY = numpy.random.normal(meanPosErrorY, stddevPosErrorY, 1)
-        randomMovZ = numpy.random.normal(meanPosErrorZ, stddevPosErrorZ, 1)
-        posError   = numpy.array([randomMovX[0], randomMovY[0], randomMovZ[0]])
+        randomMovX      = numpy.random.normal(meanPosErrorX, stddevPosErrorX, 1)
+        randomRotTheta  = numpy.random.uniform(minPlacementErrorAngle, maxPlacementErrorAngle, 1)
+        randomMovRadius = numpy.random.normal(minPlacementErrorRadius, maxPlacementErrorRadius, 1)
+
+        randomMovY      = randomMovRadius*numpy.cos(randomRotTheta)
+        randomMovZ      = randomMovRadius*numpy.sin(randomRotTheta)
+
+        posError        = numpy.array([randomMovX[0], randomMovY[0], randomMovZ[0]])
 
         # Make the new position
         IMUpos_new = IMUpos + posError
 
         # Set the new position
         IMUFrame.set_translation(opensim.Vec3(IMUpos_new))
+
+        # Rotate the IMU
+        randomRotThetaX = numpy.random.uniform(minOrientationErrorXAngle, maxOrientationErrorXAngle, 1)
+        randomRotThetaY = numpy.random.uniform(minOrientationErrorYAngle, maxOrientationErrorYAngle, 1)
+        randomRotThetaZ = numpy.random.uniform(minOrientationErrorZAngle, maxOrientationErrorZAngle, 1)
+
+        angError        = numpy.array([randomRotThetaX[0], randomRotThetaY[0], randomRotThetaZ[0]])
+
+        # Make the new orientation
+        IMUang_new = IMUang + angError
+
+        # Set the new position
+        IMUFrame.set_orientation(opensim.Vec3(IMUang_new))
+
+
 
     baseModel.finalizeConnections()
     baseModel.printToXML(modelFileName)
